b926ceb19819219647c1a6ed4e7964ce6825eb3a
wipedlifepotato <wipedlifepotato@users.noreply.github.com>
2025-09-13 21:31:25 +0700

    fix: fix time interval for tx flush

--- main.go
+++ main.go
@@ -119,7 +119,7 @@ func main() {
     go server.StartWalletSync(ctx, electrumClient, moneroClient, 30*time.Second)
     go server.StartTxBlockTransactions(ctx, electrumClient, 15*time.Second)
     
-    server.StartTxPoolFlusher(electrumClient,1*time.Second)
+    server.StartTxPoolFlusher(electrumClient,1*time.Hour)
     server.SetTxPoolBlocked(false)
     log.Println("Starting server on " + config.AppConfig.ListenAddr + ":" + config.AppConfig.Port)
     if err := s.Start(config.AppConfig.ListenAddr, config.AppConfig.Port); err != nil {
