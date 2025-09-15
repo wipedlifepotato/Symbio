221f9d31e44b681d7985e0fa272e97423f8ab853
wipedlifepotato <wipedlifepotato@users.noreply.github.com>
2025-09-15 17:32:27 +0700

    feat: pre-init go tests

---
+++ server/testutil/redis.go
@@ -0,0 +1,21 @@
+package testutil
+
+import (
+    "testing"
+
+    miniredis "github.com/alicebob/miniredis/v2"
+    "github.com/go-redis/redis/v8"
+)
+
+// NewMiniRedis starts an in-memory Redis and returns it with a client
+func NewMiniRedis(t *testing.T) (*miniredis.Miniredis, *redis.Client) {
+    t.Helper()
+    mr, err := miniredis.Run()
+    if err != nil {
+        t.Fatalf("failed to start miniredis: %v", err)
+    }
+    rdb := redis.NewClient(&redis.Options{Addr: mr.Addr()})
+    return mr, rdb
+}
+
+
